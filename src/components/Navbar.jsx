062ad28320777c775313993d25ef38444750fb9b
𝔄𝕪𝕖𝕤𝕙𝕒𝕋𝕖𝕔𝕙𝕏 <ayeshadatascientist9@gmail.com>
2025-07-01 20:17:03 +0500

    Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
             Ayesha &nbsp;
-            <span className='sm:block hidden'> | Full-Stack Developr</span>
+            <span className='sm:block hidden'> | Full-Stack Developer</span>
           </p>
         </Link>
             
